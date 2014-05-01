d85302549899bb3ce518cf8e448afcf168b031e1
ZacHack <bigzac182@yahoo.com>
2014-05-01 10:51:38 -0700

    Create MainClass.php

---
+++ AdminJoinSource/MainClass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AdminJoin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\permission\ServerOperator;
+
+class MainClass extends PluginBase implements Listener{
+
+	public function onLoad(){
+		console(TextFormat::GREEN . "AdminJoin by §aZacHack has been loaded!");
+	}
+	public function onEnable(){
+		console(TextFormat::GREEN . "onEnable() has been called");
+	}
+	
+	public function onDisable(){
+		console(TextFormat::GREEN . "onDisable() has been called");
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		console(TextFormat::YELLOW . "its working!");
+		Server::getInstance()->broadcastMessage("its working!");
+		$player = $event->getPlayer();
+		if(isOp($player) == true){
+			Server::getInstance()->broadcastMessage("Server Admin:".$event->getplayer()->getDisplayName()."");
+		}
+	}
+}
