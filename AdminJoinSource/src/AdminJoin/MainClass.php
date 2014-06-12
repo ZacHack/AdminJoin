44e88ea6aedff837a4fef032a234bda7d63ed006
ZacHack <bigzac182@yahoo.com>
2014-06-12 14:43:48 -0700

    Working version

--- AdminJoinSource/src/AdminJoin/MainClass.php
+++ AdminJoinSource/src/AdminJoin/MainClass.php
@@ -4,7 +4,6 @@ namespace AdminJoin;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\Server;
@@ -12,9 +11,6 @@ use pocketmine\permission\ServerOperator;
 
 class MainClass extends PluginBase implements Listener{
 
-	public function onLoad(){
-		console(TextFormat::GREEN . "AdminJoin by ZacHack has been loaded!");
-	}
 	public function onEnable(){
 		$this->getserver()->getPluginManager()->registerEvents($this, $this);
 	}
@@ -24,8 +20,9 @@ class MainClass extends PluginBase implements Listener{
 	
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
-		if(isOp($player) == true){
-			Server::getInstance()->broadcastMessage("Server Admin:".$event->getplayer()->getDisplayName()."");
+		$name = $player->getDisplayName();
+		if($player->isOp()){
+			Server::getInstance()->broadcastMessage("Server Op: ".$name."");
 		}
 	}
 }
