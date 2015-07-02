967ee045032ab0fa7b67930e9141a84108ee0df2
ZacHack <bigzac182@yahoo.com>
2015-07-02 12:15:13 -0700

    Added customizable message with config or command

--- AdminJoinSource/src/AdminJoin/MainClass.php
+++ AdminJoinSource/src/AdminJoin/MainClass.php
@@ -1,25 +1,51 @@
 <?php
 namespace AdminJoin;
-
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 class MainClass extends PluginBase implements Listener{
-
 	public function onEnable(){
 		$this->getserver()->getPluginManager()->registerEvents($this, $this);
+		if(!is_dir($this->getDataFolder())){
+			@mkdir($this->getDataFolder());
+		}
+		$config = new Config($this->getDataFolder()."config.yml", CONFIG::YAML, array(
+			"Message" => "Server Op: {player}",
+		));
 	}
-	
+
 	/**
 	 * @param PlayerJoinEvent $event
-	 * 
+	 *
 	 * @priority MONITOR
 	 * @ignoreCancelled true
 	 */
 	public function onJoin(PlayerJoinEvent $event){
-		if($event->getPlayer()->isOp()){
-			$this->getServer()->broadcastMessage("Server Op: " . $event->getPlayer()->getDisplayName());
+		$config = new Config($this->getDataFolder()."config.yml", CONFIG::YAML);
+			if($event->getPlayer()->isOp()){
+				$name = $event->getPlayer()->getDisplayName();
+				$message = $config->get("Message");
+				$message = str_replace("{player}", $name, $message);
+				$this->getServer()->broadcastMessage(TextFormat::GREEN.$message);
+			}
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+			case "adminjoin":
+				if(!isset($args[0])){
+					$sender->sendMessage(TextFormat::RED."Usage: ".$command->getUsage()."");
+					return true;
+				}else{
+					$config = new Config($this->getDataFolder()."config.yml", CONFIG::YAML);
+					$args = implode(" ", $args);
+					$config->set("Message", $args);
+					$sender->sendMessage(TextFormat::GREEN."[AdminJoin] You have changed the message");
+					return true;
+				}
 		}
 	}
 }
